8655e25e103c94ead4c4fba726664d19892c7542
Nills Maillet <mailletnills@gmail.com>
2026-09-23 16:20:00 +0200

    feat: add CinemaComplex class

---
+++ src/CinemaComplex.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class CinemaComplex {
+
+    private ArrayList<Cinema> cinemaList;
+
+    public CinemaComplex(ArrayList<Cinema> cinemaList) {
+        this.cinemaList = cinemaList;
+    }
+}
