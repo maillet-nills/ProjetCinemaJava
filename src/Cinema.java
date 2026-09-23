6a627e7646161105c4127ec99ad6fe6ff79e133a
Benjamin "spooltie" Bastide <spoltienet@gmail.com>
2026-09-23 16:19:35 +0200

    feat: add Cinema Class

---
+++ src/Cinema.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Cinema {
+    private String cityName;
+    private ArrayList<Room> roomList;
+
+    public Cinema(String cityName, ArrayList<Room> roomList) {
+        this.cityName = cityName;
+        this.roomList = roomList;
+    }
+}
