1cd56e7e2edfd8ff1fbcaf218d006976243e1b87
Benjamin "spooltie" Bastide <spoltienet@gmail.com>
2026-09-23 17:30:33 +0200

    feat: add attribute seatTotal in Class & Constructor

--- src/Session.java
+++ src/Session.java
@@ -2,10 +2,12 @@ public class Session {
 
     private Movie movie;
     private Room room;
+    private int seatTotal;
 
     public Session(Movie movie, Room room) {
         this.movie = movie;
         this.room = room;
+        this.seatTotal= room.getSeatTotal();
     }
 
     public String ToString(){
